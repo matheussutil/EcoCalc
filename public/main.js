505a13a2abe1a726eedbf6518b91b7486926c900
matheussutil <matheussutildesouza@gmail.com>
2026-06-22 19:39:06 +0000

    tela-Home

--- public/main.js
+++ public/main.js
@@ -1,11 +1,23 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-            e.preventDefault();
+// Função que controla a troca de telas
+        function showSection(sectionId) {
+            // 1. Esconde tudo
+            document.querySelectorAll('.section-container').forEach(section => {
+                section.classList.remove('active');
+            });
+            // 2. Mostra apenas o que o botão pediu
+            document.getElementById(sectionId).classList.add('active');
+        }
+
+        // Intercepta o envio do Login
+        document.getElementById('loginForm').addEventListener('submit', function(e) {
+            e.preventDefault(); // Impede a página de recarregar
             const email = document.getElementById('login-email').value;
-            alert('Simulação de Login para o e-mail: ' + email);
+            alert('Simulação de Login! Bem-vindo(a), ' + email);
         });
 
+        // Intercepta o envio do Cadastro
         document.getElementById('cadastroForm').addEventListener('submit', function(e) {
-            e.preventDefault();
+            e.preventDefault(); // Impede a página de recarregar
             const nome = document.getElementById('cad-nome').value;
-            alert('Simulação de Cadastro realizado para: ' + nome + '! Próximo passo: criptografar a senha no back-end.');
+            alert('Cadastro realizado para: ' + nome + '!');
         });
